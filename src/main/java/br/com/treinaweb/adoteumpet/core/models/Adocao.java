3ce2e215260c35aaa57a594bbceafd546925b716
Nadilson <nadilson@protonmail.com>
2022-06-05 01:55:30 -0300

    Criação de DTOs e Mappers de Adoção

--- src/main/java/br/com/treinaweb/adoteumpet/core/models/Adocao.java
+++ src/main/java/br/com/treinaweb/adoteumpet/core/models/Adocao.java
@@ -3,6 +3,7 @@ package br.com.treinaweb.adoteumpet.core.models;
 import java.math.BigDecimal;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -13,6 +14,7 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+@Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
